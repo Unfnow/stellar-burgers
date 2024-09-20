8f2493fe5aea179955644e14971b93ca084690ae
Unfnow <sigaevdenis13@gmail.com>
2024-09-20 21:35:16 +0300

    12 work 2.0

--- cypress/e2e/orderTest/orderTest.cy.ts
+++ cypress/e2e/orderTest/orderTest.cy.ts
@@ -11,12 +11,11 @@ describe('orderTest', () => {
     cy.get(`[data-cy='643d69a5c3f7b9001cfa0942'] button`).click(); //Добавление соуса в заказ
     cy.intercept('POST', 'api/orders', { fixture: 'order.json' }); //Перехват запроса на эндпоинт /orders
     cy.get(`[data-cy='burgerOredrBTN']`).click(); //Создание заказа
-    cy.wait(2000); // Ожидание создания заказа
+    cy.wait(1000); // Ожидание создания заказа
     const modal = cy.get('#modals');
-    modal.find(`button`).should('be.visible'); //Проверка модального окна на открытие
-    modal.find(`h2`).should('have.text', '1234');
-    modal.find(`button`).click(); //Закрытие модального окна
-    modal.should(`not.be.visible`); //Проверяется успешность закрытия модального окна
+    modal.get(`h2`).should('be.visible').and('have.text', '1234'); //Проверка модального окна на открытие и проверка подставных данных заказа
+    modal.get(`.b_7mdKCbZ9NwpuNYvKgW button`).click(); //Закрытие модального окна
+    cy.get('#modals').should(`not.be.visible`); //Проверяется успешность закрытия модального окна
     cy.get(`[data-cy='constructor-bun']`).should('have.text', 'Выберите булки'); //Проверка конструктора на пустоту
     cy.get(`[data-cy='constructor-main']`).should(
       'have.text',
